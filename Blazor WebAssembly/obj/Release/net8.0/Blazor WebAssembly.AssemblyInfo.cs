b6c568218bd42d84bb4844f15c04a23a7e0833ce
israel <israel094@hotmail.com>
2025-05-22 17:51:53 -0300

    implementacao

--- Blazor WebAssembly/obj/Release/net8.0/Blazor WebAssembly.AssemblyInfo.cs
+++ Blazor WebAssembly/obj/Release/net8.0/Blazor WebAssembly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor WebAssembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50c728d381767df71f5e919cc3cfaa97dc22f21a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e55f267a85bdb1b4bf3faa34b83f72b3df7fd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor WebAssembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor WebAssembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
