4d9178e9cfa3996827ef3f04102b2461c3f644ce
israel <israel094@hotmail.com>
2025-06-30 22:20:04 -0300

    v1.5.3 - correcao: correcao na ctions/upload-artifact para v4

--- Blazor WebAssembly/obj/Debug/net8.0/Blazor WebAssembly.AssemblyInfo.cs
+++ Blazor WebAssembly/obj/Debug/net8.0/Blazor WebAssembly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor WebAssembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4157d387bd46f03baaf52070a3c3b532641f517")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b35a023263bcfbb74c11c1a0476015809d03f312")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor WebAssembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor WebAssembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
