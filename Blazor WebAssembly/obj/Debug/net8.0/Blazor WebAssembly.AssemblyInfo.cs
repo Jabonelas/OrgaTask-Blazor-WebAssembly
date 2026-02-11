6fc48e8ba802ce43a579c8f5592f40529424958a
israel <israel094@hotmail.com>
2026-02-11 18:42:59 -0300

    salvando ultimas alteracoes

--- Blazor WebAssembly/obj/Debug/net8.0/Blazor WebAssembly.AssemblyInfo.cs
+++ Blazor WebAssembly/obj/Debug/net8.0/Blazor WebAssembly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor WebAssembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dda859c9e844bdeea0bc362b40c550ac2b86a971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b654581b3e3549ca3454318d76e4de64e37a09de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor WebAssembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor WebAssembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
